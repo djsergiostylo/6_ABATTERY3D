970d7299dc076a5bfb197fead5bd25ef4387826a
djsergiostylo <djsergiostylo@gmail.com>
2026-08-31 12:22:52 +0200

    Copy Kotlin from repo 5 feat/v2-monetized: Color

---
+++ app/src/main/java/com/stylo/batterymonitor/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.stylo.batterymonitor.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BatteryGreen = Color(0xFF66BB6A)
+val ThermalOrange = Color(0xFFFF8A3D)
+val CardSurface = Color(0xFF1E1E1E)
+val Background = Color(0xFF121212)
