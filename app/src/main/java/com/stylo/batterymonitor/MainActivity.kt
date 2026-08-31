8fe848df4ad594c6f8ab449c3ca6ef5db6e15759
djsergiostylo <djsergiostylo@gmail.com>
2026-08-31 19:31:38 +0200

    Fix MainActivity package to match application namespace

--- app/src/main/java/com/stylo/batterymonitor/MainActivity.kt
+++ app/src/main/java/com/stylo/batterymonitor/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.stylo.batterymonitor
+package com.stylo.abattery3d
 
 import android.Manifest
 import android.content.pm.PackageManager
@@ -14,7 +14,6 @@ import androidx.core.app.ActivityCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.gms.ads.MobileAds
 import com.stylo.batterymonitor.ads.AdBannerComposable
-import com.stylo.batterymonitor.ads.AdMobManager
 import com.stylo.batterymonitor.ui.BatteryViewModel
 import com.stylo.batterymonitor.ui.BatteryWebViewScreen
 import com.stylo.batterymonitor.ui.theme.StyloBatteryMonitorTheme
